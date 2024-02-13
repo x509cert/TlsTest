0f15a9ee98b9e3d7c9c8b7cd0633f346f580015d
Administrator <Administrator@WIN-GREBH3TU51A>
2024-02-13 14:15:50 -0800

    Add project files.

---
+++ TlsTest/Program.cs
@@ -0,0 +1,46 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// Sample code to test a SQL Server instance for TLS protocol version support
+// Written by Michael Howard, Azure Data Platform security team
+////////////////////////////////////////////////////////////////////////////////
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+
+string host = "localhost";
+int port = 1433;
+
+// this does nothing in .NET Core
+//ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11;
+
+SslProtocols[] tlsVersions = [SslProtocols.Ssl2, SslProtocols.Ssl3, SslProtocols.Tls, SslProtocols.Tls11, SslProtocols.Tls12, SslProtocols.Tls13];
+
+foreach (var prot in tlsVersions)
+{
+    Console.WriteLine($"Trying {prot}");
+    try
+    {
+        TcpClient client = new(host, port);
+
+        using (SslStream sslStream = new SslStream(client.GetStream(),
+            false,
+            (sender, certificate, chain, sslPolicyErrors) => true,  // we don't care about the cert only checking protocol support
+            null)) {
+
+            var options = new SslClientAuthenticationOptions {
+                TargetHost = host,
+                EnabledSslProtocols = prot
+            };
+            sslStream.AuthenticateAsClient(options);
+
+            Console.WriteLine($"SSL/TLS protocol: {sslStream.SslProtocol}");
+            Console.WriteLine($"Ciphersuite: {sslStream.CipherAlgorithm} {sslStream.HashAlgorithm}");
+
+            client.Close();
+        }
+    }
+    catch (Exception e) {
+        Console.WriteLine($"Exception: {e.InnerException.Message}");
+    }
+
+    Console.WriteLine();
+}
